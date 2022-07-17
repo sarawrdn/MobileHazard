499196a91f7d14bccf3f2fb9cf85f230586003c2
mirayzd <aqilahyazid08@gmail.com>
2022-07-18 03:28:15 +0800

    add

--- app/src/main/java/com/example/test/MapsFragment.java
+++ app/src/main/java/com/example/test/MapsFragment.java
@@ -1,21 +1,188 @@
 package com.example.test;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.test.databinding.ActivityMapsBinding;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Vector;
 
 
-public class MapsFragment extends Fragment {
+public class MapsFragment extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    MarkerOptions marker;
+    LatLng centerlocation;
+
+    Vector<MarkerOptions> markerOptions;
+
+    private String URL="http://myhazard.epizy.com/index.php";
+    RequestQueue requestque;
+    Gson gson;
+    Information[] informations;
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //gson= new GsonBuilder().create();
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapsFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapsFragment);
+        mapsFragment.getMapAsync(this);
+
+        centerlocation = new LatLng(3.0,101);
+
+        markerOptions = new Vector<>();
+
+
     }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
+        // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+
+        for(MarkerOptions mark: markerOptions) {
+            mMap.addMarker(mark);
+        }
+
+        enableMyLocation();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(centerlocation,6));
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        sendRequest();
+    }
+
+
+    private void enableMyLocation() {
+
+        String perms[] = {"android.permission.ACCESS_FINE_LOCATION","android.permission.ACCESS_NETWORK_STATE"};
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            if (mMap != null) {
+                mMap.setMyLocationEnabled(true);
+                Log.d("Maps","permission granted");
+            }
+        } else {
+            // Permission to access the location is missing. Show rationale and request permission
+
+            Log.d("Maps","permission denied");
+            ActivityCompat.requestPermissions(this,perms ,200);
+
+        }
+
+    }
+    public void onMapSearch(View view) {
+        EditText locationSearch = (EditText) findViewById(R.id.editText);
+        String location = locationSearch.getText().toString();
+        List<Address> addressList = null;
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(),
+                    address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18));
+        }
+    }
+
+    public void sendRequest(){
+        requestque = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,URL,onSuccess,onError);
+        requestque.add(stringRequest);
+    }
+
+    public Response.Listener<String> onSuccess = new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+
+            informations = gson.fromJson(response,Information[].class);
+
+            Log.d("Maklumat","Number of Information Data Point :" + informations.length);
+
+            if (informations.length < 1){
+                Toast.makeText(getApplicationContext(),"Problem retrieving ",Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            for (Information info: informations) {
+                Double lat = Double.parseDouble(info.lat);
+                Double lng = Double.parseDouble(info.lng);
+                String title = "Hazard : "+ info.hazard;
+                //   String snippet= info.time;
+                //  String snippets = info.date;
+                String snippet= "Reported by : "+ info.people;
+
+                MarkerOptions marker = new MarkerOptions().title(title)
+                        .position(new LatLng(lat,lng))
+                        .snippet(snippet)
+                        //.snippet(snippets)
+                        //  .snippet(snippetss)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+
+                mMap.addMarker(marker);
+
+            }
+
+        }
+    };
+    public Response.ErrorListener onError = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Toast.makeText(getApplicationContext(),error.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    };
 }
